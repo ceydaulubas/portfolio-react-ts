e9bdf5fa4302732e7317ccc53795c08d99b335eb
ceydaulubas <ceyda.ulubas@gmail.com>
2024-10-07 12:07:32 +0300

    Add pagination for articles part

--- src/pages/Articles/Articles.tsx
+++ src/pages/Articles/Articles.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+import { Pagination } from "antd";
 import { mediumArticles } from "../../assets/copy/mediumArticles";
 import { ArticlesContainer, ArticleBox, ArticleImage, ArticleInfo, ArticleTitle, ArticleSubject } from "./Article.sc";
 
@@ -10,18 +12,42 @@ interface Article {
 }
 
 const Articles = () => {
+  // Create state for determining which articles will be shown on each page.
+  const [currentPage, setCurrentPage] = useState(1);
+  const [pageSize] = useState(4); // Sayfa başına gösterilecek makale sayısı
+
+  // Calculate the starting and ending points of the articles
+  const startIndex = (currentPage - 1) * pageSize;
+  const endIndex = startIndex + pageSize;
+  // Filter the articles to be shown on the current page
+  const currentArticles = mediumArticles.slice(startIndex, endIndex); 
+
+  // Change the current page when the user clicks on the pagination component
+  const handlePageChange = (page: number) => {
+    setCurrentPage(page);
+  };
+
   return (
-    <ArticlesContainer>
-      {mediumArticles.map((item: Article) => (
-        <ArticleBox key={item.id} href={item.website} target="_blank" rel="noopener noreferrer">
-          <ArticleImage src={item.photo} alt="" />
-          <ArticleInfo>
-            <ArticleTitle>{item.title}</ArticleTitle>
-            <ArticleSubject>{item.subject}</ArticleSubject>
-          </ArticleInfo>
-        </ArticleBox>
-      ))}
-    </ArticlesContainer>
+    <>
+      <ArticlesContainer>
+        {currentArticles.map((item: Article) => (
+          <ArticleBox key={item.id} href={item.website} target="_blank" rel="noopener noreferrer">
+            <ArticleImage src={item.photo} alt="" />
+            <ArticleInfo>
+              <ArticleTitle>{item.title}</ArticleTitle>
+              <ArticleSubject>{item.subject}</ArticleSubject>
+            </ArticleInfo>
+          </ArticleBox>
+        ))}
+      </ArticlesContainer>
+      <Pagination
+        current={currentPage}
+        pageSize={pageSize}
+        total={mediumArticles.length}
+        onChange={handlePageChange}
+        style={{ marginTop: "20px", textAlign: "center" }} 
+      />
+    </>
   );
 };
 
